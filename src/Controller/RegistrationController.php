0a8db0dee76e4717da312f6da8d2fb1c3a85a1f5
PiosX <piosso.pios@op.pl>
2021-08-28 18:02:31 +0200

    Login page unc.

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/register', name: 'register')]
+class RegistrationController extends AbstractController
+{
+    #[Route('/emailsignup', name: 'emailsignup')]
+    public function index(): Response
+    {
+        return $this->render('registration/index.html.twig', [
+            'controller_name' => 'RegistrationController',
+        ]);
+    }
+}
